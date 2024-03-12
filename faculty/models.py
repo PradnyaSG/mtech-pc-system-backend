d851c50dffc07d667089c4745435da22f71ee937
PradnyaSG <pradnyasg1601@gmail.com>
2024-03-12 20:22:05 +0530

    Faculty model created with a function to dispaly one intialized faculty values in html template

--- faculty/models.py
+++ faculty/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class faculty(models.Model):
+    name = models.CharField(max_length=255)
+    dept=models.CharField(max_length=255)
+    #counters for each role
+    isguide = models.IntegerField(default=0)#guide
+    ischair = models.IntegerField(default=0)#chair person
+    iscommem = models.IntegerField(default=0)#committee member
+    isprojcoo=models.IntegerField(default=0)#project coordinator
+    email = models.CharField(max_length=9,unique=True)
+    domain=models.CharField(max_length=255)
+
+
--- faculty/views.py
+++ faculty/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from django.contrib import messages
+#from .forms import ExcelUploadForm
+from .models import *
+# 
+def addfaculty(request):
+     #Creating a faculty object
+     facobj=faculty(name="ABC",dept="CSE", isguide=1,ischair=2 , iscommem=3,isprojcoo=1,email = "abc@gmail.com",domain="CV")
+     facobj.save()
+     return render(request,'home.html',{'facobj':facobj})
