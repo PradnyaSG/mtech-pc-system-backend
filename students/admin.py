44bd4b3d6d3375973ccccac9a7c42944b2c81318
akhilnerella81 <akhilnerella81@gmail.com>
2024-03-12 17:53:26 +0530

    admin_import functionality & student model

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from .models import Student
 
-# Register your models here.
+class StudentResource(resources.ModelResource):
+    class Meta:
+        model = Student
+
+class StudentAdmin(ImportExportModelAdmin):
+    resource_class = StudentResource
+
+admin.site.register(Student, StudentAdmin)
+
+
--- students/views.py
+++ students/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+from django.contrib import messages
+#from .forms import ExcelUploadForm
+from .models import Student
+#import pandas as pd
 def students(request):
+    
     return HttpResponse("Hello World!!!")
+
